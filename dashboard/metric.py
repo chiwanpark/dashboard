a78d8745fdcea5f96f9870ecb45bcb936dc19b7a
Chiwan Park <chiwanpark@icloud.com>
2019-02-01 23:19:02 +0900

    Enable gzip encoding for metric collector API

--- dashboard/metric.py
+++ dashboard/metric.py
@@ -2,9 +2,25 @@ from dashboard.storage import get_secure_key, store_server_metric
 from flask import current_app as app
 from flask import Blueprint, abort, jsonify, make_response, request
 from time import time
+import json
+import zlib
 
 
 blueprint = Blueprint('metric', __name__, url_prefix='/metric')
+
+
+@blueprint.after_request
+def _after_request(response):
+    valid = 'gzip' in request.headers.get('Accept-Encoding', '')
+    valid &= 200 <= response.status_code and response.status_code < 300
+
+    if valid:
+        app.logger.info('Compress response via zlib')
+        response.data = zlib.compress(response.data, 9)
+        response.headers['Content-Encoding'] = 'gzip'
+        response.headers['Content-Length'] = len(response.data)
+
+    return response
 
 
 def is_secure_request(hostname):
@@ -26,7 +42,13 @@ def add_metric(hostname):
     if not is_secure_request(hostname):
         abort(401)
 
-    data = request.get_json()
+    if request.headers.get('Content-Encoding', '') == 'gzip':
+        try:
+            data = json.loads(zlib.decompress(request.data))
+        except (ValueError, zlib.error):
+            data = None
+    else:
+        data = request.get_json()
     if not data:
         abort(400)
     timestamp = time()
